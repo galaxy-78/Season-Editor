415d54521743cd1a4f9bd6f15cd2655e2da3754c
galaxy-78 <magry78@gmail.com>
2026-01-06 14:28:43 +0900

    chore: fix action run error

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,27 +1,28 @@
 import typescriptEslint from "typescript-eslint";
+import prettier from "eslint-config-prettier";
 
-export default [{
+export default [
+  {
     files: ["**/*.ts"],
-}, {
     plugins: {
-        "@typescript-eslint": typescriptEslint.plugin,
+      "@typescript-eslint": typescriptEslint.plugin,
     },
-
     languageOptions: {
-        parser: typescriptEslint.parser,
-        ecmaVersion: 2022,
-        sourceType: "module",
+      parser: typescriptEslint.parser,
+      ecmaVersion: 2022,
+      sourceType: "module",
     },
-
     rules: {
-        "@typescript-eslint/naming-convention": ["warn", {
-            selector: "import",
-            format: ["camelCase", "PascalCase"],
-        }],
+      "@typescript-eslint/naming-convention": [
+        "warn",
+        { selector: "import", format: ["camelCase", "PascalCase"] },
+      ],
 
-        curly: "warn",
-        eqeqeq: "warn",
-        "no-throw-literal": "warn",
-        semi: "warn",
+      curly: "off",
+      eqeqeq: "off",
+      "no-throw-literal": "warn",
     },
-}];
+  },
+
+  prettier,
+];
